09925b9a52993e591e8400d246454238b358f1c3
jithjayan <jithjayan9341@gmail.com>
2024-09-03 09:10:08 +0530

    work strart

---
+++ 29.08.24/project.py
@@ -0,0 +1,22 @@
+shope=[]
+while True:
+    print('''
+          1.Registration
+          2.View
+          3.Update
+          4.Delete
+          5.Search
+          ''')
+    num=int(input("Enter the input"))
+    if num==1:
+        while True:
+            print('''
+                  1.tyre
+                  2.customer
+                  3.employee
+                  4.exit''')
+            num=int(input("Enter the input"))   
+            if num==1:
+                
+
+        
