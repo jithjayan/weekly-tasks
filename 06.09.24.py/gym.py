7ff99fb07b6900c3cb49a1592f005a30a4718355
jithjayan <jithjayan9341@gmail.com>
2024-09-12 09:08:22 +0530

    updates

--- 06.09.24.py/gym.py
+++ 06.09.24.py/gym.py
@@ -1,4 +1,4 @@
-shop=[{"cname":'hufu',"vnum":11,"phnum":3534,"washtype":'full',"wprice":10000}]
+gym=[{"cname":'hufu',"vnum":11,"phnum":3534,"washtype":'full',"wprice":10000}]
 while True:
     print('''
           1.Registration
@@ -12,38 +12,40 @@ while True:
     num=int(input("Enter the input"))
     if num==1:
         cname=str(input("Enter customer name"))
-        vnum=input("Enter vehicle number")
+        age=int(input("Enter the age"))
         phnum=int(input("Enter phone number"))
+        print("Gender:")
+        print("Select plan")
         while True:
             print(''' 
-            1.full wash
-            2.exterior
-            3.interior''')
+            1.monthly
+            2.6 month
+            3.yearly''')
             n=int(input("enter the value"))
             if n==1:
                 price=10000
-                type='full'
+                type='monthly'
                 break
             elif n==2:
                 price=5000
-                type='exterior'
+                type='6 month'
                 break
             elif n==3:
                 price=7000
-                type='interior'
+                type='yearly'
                 break
             else:
                 print("Invalid choice")
-        washtype=type 
-        wprice=price      
-        shop.append({"cname":cname,"vnum":vnum,"phnum":phnum,"washtype":washtype,"wprice":price})
+        plan=type 
+        amount=price      
+        gym.append({"cname":cname,"phnum":phnum,"plan":plan,"price":price})
     elif num==2:
-         for i in shop:
+         for i in gym:
             print(i)
     elif num==3:
          a=int(input('enter vehicle number'))
          f=0
-         for i in shop:
+         for i in gym:
               if i["vnum"]==a:
                    nprice=input("enter new price")
                    i['wprice']=nprice
@@ -53,16 +55,16 @@ while True:
     elif num==4:
         a=int(input("Enter vehicle number"))
         f=0
-        for i in shop:
+        for i in gym:
             if i['vnum']==a:
-                shop.remove(i)
+                gym.remove(i)
                 f=1
         if f==0:
                 print(" not found")
     elif num==5:
         a=int(input("Enter vehicle number"))
         f=0
-        for i in shop:
+        for i in gym:
             if i['vnum']==a:
                 print(i)
                 f=1
@@ -72,7 +74,7 @@ while True:
     elif num==6:
         z=int(input("v num"))
         f=0
-        for i in shop:
+        for i in gym:
             if i["vnum"]==z:
                 print(i['cname'],i['vnum'],i['phnum'],i['washtype'])
                 print('Amount payable') 
