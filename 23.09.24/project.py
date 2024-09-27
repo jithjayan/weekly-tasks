d11cc00920b721c899130edd76f6912963d76bd2
jithjayan <jithjayan9341@gmail.com>
2024-09-27 09:10:23 +0530

    edit

--- 23.09.24/project.py
+++ 23.09.24/project.py
@@ -16,10 +16,23 @@ def register():
             register()
     if f==0:
         name=str(input('enter your name : '))
+        age=int(input('enter your age : '))
         phone=int(input('enter your number : '))
         password=input('enter the password : ')
         print('Registration Succesfull email id is your username')
-        user.append({'id':id,'name':name,'email':email,'phone':phone,'password':password,'book':[]})
+        user.append({'id':id,'name':name,'age':age,'email':email,'phone':phone,'password':password,'book':[]})
+def login():
+    usern=str(input('Enter Username : '))
+    passw=input('Enter password : ')
+    f=0
+    u=''
+    if usern=='admin' and passw=='admin':
+        f=1
+    for i in user:
+        if usern==i['email'] and passw==i['password']:
+            f=2
+            u=i
+    return f,u
 
 
 
@@ -32,4 +45,58 @@ while True:
     if ch==1:
         register()
     elif ch==2:
-       
+       f,u=login()
+       if f==1:
+            while True:
+                print('''
+                1.Add book
+                2.View Book
+                3.Update Book Details
+                4.Delete book
+                5.View Users
+                6.Logout''')
+
+                c1=int(input('enter your choice : '))
+                if c1==1:
+                    add_bk()
+                elif c1==2:
+                    view_bk()
+                elif c1==3:
+                    update_bk()
+                elif c1==4:
+                    delete_bk()
+                elif c1==5:
+                    view_usr()
+                elif c1==6:
+                    break
+                else:
+                    print('invalid Choice')
+        elif f==2:
+            while True:
+                print('''
+                    1.view profile
+                    2.view book
+                    3.update profile
+                    4.Rent a book
+                    5.Return a book
+                    6.Books in hand
+                    7.logout''')
+                c1=int(input('enter your choice : '))
+                if c1==1:
+                    view_pro(u)
+                elif c1==2:
+                    view_bk()
+                elif c1==3:
+                    update_usr(u)
+                elif c1==4:
+                    rent(u)
+                elif c1==5:
+                    return_bk(u)
+                elif c1==6:
+                    bkhnd(u)
+                elif c1==7:
+                    break
+                else:
+                    print('invalid option')
+        else:
+            print('invalid username or password')       
